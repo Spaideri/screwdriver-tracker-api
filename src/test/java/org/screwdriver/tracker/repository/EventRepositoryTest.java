051d7fcdb3cee38a8f4a6092f7d36900186cc09b
Juho Rautio <juho.rautio@zebit.fi>
2015-04-08 21:45:47 +0300

    Fixing timestamps

--- src/test/java/org/screwdriver/tracker/repository/EventRepositoryTest.java
+++ src/test/java/org/screwdriver/tracker/repository/EventRepositoryTest.java
@@ -45,6 +45,6 @@ public class EventRepositoryTest {
     @Test
     public void shouldFindLatestEventByTrackerId() {
         Event event = eventRepository.findTop1ByTrackerIdOrderByEventTimestampDesc(1L);
-        assertEquals(new DateTime(LATEST_TIMESTAMP).getMillis(), event.getEventTimestamp().getTime());
+        assertEquals(new DateTime(LATEST_TIMESTAMP).toDate(), event.getEventTimestamp());
     }
 }
